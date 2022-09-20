87be5d3b6536559c619ed6dc5c032bbabf847c8b
Alexander Grötzsch <j.k0201@gmx.de>
2022-09-20 11:28:27 +0000

    fixed img colors

--- src/mlx_pixel_handler.c
+++ src/mlx_pixel_handler.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   mlx_pixel_handler.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: skorte <skorte@student.42wolfsburg.de>     +#+  +:+       +#+        */
+/*   By: agrotzsc <agrotzsc@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 17:15:08 by skorte            #+#    #+#             */
-/*   Updated: 2022/09/19 17:23:17 by skorte           ###   ########.fr       */
+/*   Updated: 2022/09/20 11:25:58 by agrotzsc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,20 +39,21 @@ int	extract_pixel(t_buff *buffer, int x, int y)
 	int	pixel;
 	int	color;
 
+	color = 0;
 	pixel = (y * buffer->line_bytes) + (x * 4);
 	if (buffer->endian == 1)
 	{
-		color = buffer->buffer[pixel + 0];
-		color += buffer->buffer[pixel + 1] >> 8;
-		color += buffer->buffer[pixel + 2] >> 16;
-		color += buffer->buffer[pixel + 3] >> 24;
+		color = buffer->buffer[pixel + 0] << 24;
+		color += buffer->buffer[pixel + 1] << 16;
+		color += buffer->buffer[pixel + 2] << 8;
+		color += buffer->buffer[pixel + 3];
 	}
 	else
 	{
 		color = buffer->buffer[pixel + 0];
-		color += buffer->buffer[pixel + 1] >> 8;
-		color += buffer->buffer[pixel + 2] >> 16;
-		color += buffer->buffer[pixel + 3] >> 24;
+		color += buffer->buffer[pixel + 1] << 8;
+		color += buffer->buffer[pixel + 2] << 16;
+		color += buffer->buffer[pixel + 3] << 24;
 	}
 	return (color);
 }
